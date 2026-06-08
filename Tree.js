546d335361a88874ad72cb76b2dcfdf475e1990e
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-06-08 11:11:32 -0700

    feat: find the mid and create a Node that will become the root of the array

--- Tree.js
+++ Tree.js
@@ -1,3 +1,5 @@
+import Node from "./Node.js";
+
 class Tree {
   constructor(arr) {
     this.root = this.#buildTree(arr);
@@ -7,12 +9,14 @@ class Tree {
     // TODO: implement function
     const sortedArr = arr.sort((a, b) => a - b);
 
-    const finalArr = [...new Set(sortedArr)];
+    const mid = Math.floor(sortedArr.length / 2);
+    const root = new Node(sortedArr[mid]);
 
-    console.log(finalArr);
+    console.log(sortedArr);
+    return root;
   }
 }
 
-const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+const tree = new Tree([1, 7, 4, 23, 8, 9, 3, 5, 67, 88, 99]);
 
-tree.root;
+console.log(tree.root);
