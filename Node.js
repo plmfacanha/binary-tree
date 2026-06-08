a62424e2b2c7886f95817810c5f5357e979ecc01
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-06-08 10:41:38 -0700

    fix: Node.js with only data as parameter and attributes this.left and this.right

--- Node.js
+++ Node.js
@@ -1,7 +1,7 @@
 class Node {
-  constructor(data, left = null, right = null) {
+  constructor(data) {
     this.data = data;
-    this.left = left;
-    this.right = right;
+    this.left = null;
+    this.right = null;
   }
 }
