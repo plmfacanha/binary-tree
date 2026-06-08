b0131ec67f07c6205b5bbf74325d1bad928ee781
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-06-08 10:54:43 -0700

    feat: sorted and remove duplicates from the arr passed in

--- Tree.js
+++ Tree.js
@@ -5,5 +5,14 @@ class Tree {
 
   buildTree(arr) {
     // TODO: implement function
+    const sortedArr = arr.sort((a, b) => a - b);
+
+    const finalArr = [...new Set(sortedArr)];
+
+    console.log(finalArr);
   }
 }
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+tree.root;
