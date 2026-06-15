a94bcb7f10b2fd28ca23bc5923b9610fcb76fbec
Pedro Facanha <plmfacanha.dev@gmail.com>
2026-06-14 21:48:08 -0700

    feat: went back to the implementation to understand better what was going on

--- Tree.js
+++ Tree.js
@@ -146,18 +146,12 @@ const prettyPrint = (node, prefix = "", isLeft = true) => {
   prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
 };
 
-const tree = new Tree([15]);
+const tree = new Tree([4]);
 
 // tree.insert(11);
 // tree.insert(10);
 tree.insert(3);
 tree.insert(2);
-tree.insert(6);
-tree.insert(5);
-tree.insert(11);
-tree.insert(9);
-tree.insert(7);
-tree.insert(8);
 tree.insert(1);
-tree.deleteItem(15);
+tree.deleteItem(4);
 prettyPrint(tree.root);
